c20334088885b81654c9382e34e737ddd72742bd
JuanFirmansyah <juanfirmansyah.jf@gmail.com>
2022-04-12 16:32:57 +0700

    tugas3

---
+++ application/views/v_footer.php
@@ -0,0 +1,12 @@
+<head>
+    <link rel="stylesheet" type="text/css" href="<?php echo base_url() 
+?>assets/css/stylebuku.css">
+</head>
+
+<footer>
+    <a href=”http://www.RentalBuku.com”>RentalBuku</a>
+</footer>
+</div>
+</body>
+
+</html>
